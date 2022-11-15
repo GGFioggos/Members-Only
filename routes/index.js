f8c060370d8c4fe50aeed047646269e58620d61c
moschidisg <mosxidismosxidis@gmail.com>
2022-11-15 14:41:31 +0200

    populate router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const signUpController = require('../controllers/signUpController');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
 });
+
+router.get('/sign-up', function (req, res, next) {
+    res.render('sign_up', { title: 'Sign Up' });
+});
+
+router.post('/sign-up', signUpController.sign_up);
 
 module.exports = router;
